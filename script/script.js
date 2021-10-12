90a19b93bcb261b298e94ff60e3f2a73613594a0
dm-marina <mysummer1404@gmail.com>
2021-10-12 16:37:15 +0300

    dm-marina
    hw3

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// Task 1
+let name = "John";
+let admin = name;
+alert(admin);
+
+// Task2
+let earth; //переменная для названия нашей планеты
+let currentUser; //переменная для хранения текущего посетителя сайта
+
+// Task3
+let str = "10" + 10;
+console.log(str, typeof(str));
+
